2837c22e8442fc5299a40214e37852dc68f07090
Jimin-96 <wlalsdl684@naver.com>
2024-09-09 11:15:08 +0900

    asdf

---
+++ src/test/java/com/javaex/MiniprojectCatshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniprojectCatshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
